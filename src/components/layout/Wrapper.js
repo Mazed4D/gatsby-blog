7302615d0d40324eb12b728ab56063ce786378b8
Mazed4D <playmyfiddleman@gmail.com>
2022-01-22 17:13:15 +0100

    Added react helmet

--- src/components/layout/Wrapper.js
+++ src/components/layout/Wrapper.js
@@ -3,10 +3,16 @@ import '../../styles/global.scss';
 import { layout } from './Wrapper.module.scss';
 import Footer from './Footer';
 import TopNav from './TopNav';
+import Helmet from 'react-helmet';
 
 const Wrapper = ({ children }) => {
 	return (
 		<div className={layout}>
+			<Helmet>
+				<meta charSet='utf-8' />
+				<title>Milan's Blog</title>
+				<link rel='canonical' href='http://mysite.com/example' />
+			</Helmet>
 			<TopNav />
 			{children}
 			<Footer />
